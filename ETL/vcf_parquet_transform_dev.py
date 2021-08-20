7e85525dc92f2ab42d58e0fe0c00794a9dd02a82
Rahul Chaturvedi <rahulch@amazon.com>
2021-08-19 19:01:07 -0700

    etl changes

--- ETL/vcf_parquet_transform_dev.py
+++ ETL/vcf_parquet_transform_dev.py
@@ -1,4 +1,4 @@
-#pyspark script to convert VCF to parquet 
+#pyspark script to convert VCF to parquet
 
 import hail as hl
 from bokeh.io import show, output_notebook
@@ -18,16 +18,16 @@ for i in sample_id:
 	filename = 's3://ab3/dev_input_vcf/' + i + '/' + i + '.hard-filtered.vcf.bgz'
 	#if "additional_698_related" in filename:
 	#	continue
-	#else: 
+	#else:
 		#sample_id = os.path.basename(filename).replace(".hard-filtered.vcf.bgz","")
-	sample_id_=str(i.split('/')[-1].split('.')[0])	
+	sample_id_=str(i.split('/')[-1].split('.')[0])
 	vds=hl.import_vcf(filename,reference_genome='GRCh38')
 	variant_table = vds.make_table()
 	v_spark=variant_table.to_spark()
 	v_spark_renamed=v_spark.toDF("locus.contig","locus.position","alleles","rsid","qual","filters",'info.AC','info.AF','info.AN','info.DB','info.DP','info.END','info.FS','info.FractionInformativeReads','info.LOD','info.MQ','info.MQRankSum','info.QD','info.R2_5P_bias','info.ReadPosRankSum','info.SOR',"AD","AF","DP","FIR2","F2R1","GP","GQ","GT.alleles","GT.phased","MB","PL","PRI","PS","SB","SQ")
 	v_spark_renamed=v_spark_renamed.withColumn("sample_id",lit(sample_id_))
 	v_spark_renamed=v_spark_renamed.withColumn('ref',v_spark_renamed.alleles[0]).withColumn('alt',v_spark_renamed.alleles[1])
-	v_spark_renamed = v_spark_renamed.withColumnRenamed("locus.contig","chrom").withColumnRenamed("locus.position","pos")	
+	v_spark_renamed = v_spark_renamed.withColumnRenamed("locus.contig","chrom").withColumnRenamed("locus.position","pos")
 	v_spark_renamed.createOrReplaceTempView("variant_v")
-	df=spark.sql("SELECT concat(chrom, ':', ref, ':', alt, ':', cast(pos as string)) as variant_id,* FROM variant_v where length(chrom)<6")
+	df=spark.sql("SELECT * FROM variant_v where length(chrom)<6")
 	df.write.parquet('s3://ab3/dev_output_parquet/'+sample_id_+'/')
